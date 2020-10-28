6d5a90914d88051810a87b754d87bde92648e76d
Gleb Ermakov <ermakoff.gleb15@gmail.com>
2020-10-28 11:58:20 +0300

    bulls-cows2

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,84 +2,105 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
-        int level=0, userCount, userNumbers=0, div, num, cows=0,bulls=0,index=0,countTry=0;
+        int level=0, userCount,countTry=0, userNumbers=0;
         String userString;
-        String correctString;
+        String correctString="";
         boolean gameOver =false;
 
         Scanner scanner = new Scanner(System.in);
         Scanner string = new Scanner(System.in);
         System.out.println("Введите сложность игры");
-        if(scanner.hasNextInt()){
-            level = scanner.nextInt();
-            if(level>=3&&level<=5){
-                int[] numbers = new int[level];
-                numbers[0]=(int)(Math.random()*10);
-                correctString=String.valueOf(numbers[0]);
 
-                for(int i=1;i<level;i++){
-                    numbers[i]=(int)(Math.random()*10);
-                    for(int j=0;j<i;j++){
-                        if(numbers[j]==numbers[i]){
-                            numbers[i]=(int)(Math.random()*10);
-                            j=-1;
-                        }
-                    }
-                    correctString=correctString+String.valueOf(numbers[i]);
-                }
-
-                System.out.println("Попробуйте отгадать последовательность");
-                while (!gameOver) {
-                    if (string.hasNextInt()) {
-                        userString = string.nextLine();
-                        userCount = userString.length();
-                        userNumbers = Integer.parseInt(userString);
-                        if (userCount == level) {
-                            bulls = 0;
-                            cows = 0;
-                            countTry++;
-                            for (int i = 0; i < level; i++) {
-                                div = level-1;
-                                num = userNumbers;
-                                index = 0;
-                                while (div != -1) {
-                                    if (numbers[i] == (int)(num / Math.pow(10, div))) {
-                                        if (i == index) {
-                                            bulls++;
-                                        } else {
-                                            cows++;
-                                        }
-                                    }
-                                    num = num - (int)((int)(num / Math.pow(10, div)) * Math.pow(10, div));
-                                    div--;
-                                    index++;
-                                }
-                            }
-                            if (bulls == level) {
-                                System.out.println("Вы победили! Загаданное число- " + correctString+", количество попыток - "+countTry);
-                                gameOver=true;
-                            } else {
-                                System.out.println("Попробуйте еще раз, количество коров - " + cows + " количество быков - " + bulls);
-                            }
-                        } else {
-                            System.out.println("Нерпавильная длина, попробуйте еще раз");
-                        }
-                    } else {
-                        userString = string.nextLine();
-                        if (userString.equalsIgnoreCase("сдаюсь")) {
-                            System.out.println("Последовательность была: " + correctString);
-                            gameOver=true;
-                        }else{
-                            System.out.println("Введите последовательность цифр или сдаюсь");
-                        }
-                    }
-                }
-            }else{
-                System.out.println("Выход из программы");
-            }
-        }else{
+        if(!scanner.hasNextInt()){
             System.out.println("Выход из программы");
+            return;
         }
 
+        level = scanner.nextInt();
+
+        if(level<3||level>5){
+            System.out.println("Выход из программы");
+            return;
+        }
+
+        int[] numbers = Generation(level);
+        correctString=CorrectString(level,numbers);
+        System.out.println("Попробуйте отгадать последовательность");
+
+        while (!gameOver) {
+            if (!string.hasNextInt()) {
+                userString = string.nextLine();
+                if (userString.equalsIgnoreCase("сдаюсь")) {
+                    System.out.println("Последовательность была: " + correctString);
+                    gameOver=true;
+                }else{
+                    System.out.println("Введите последовательность цифр или сдаюсь");
+                }
+                continue;
+            }
+            userString = string.nextLine();
+            userCount = userString.length();
+            userNumbers = Integer.parseInt(userString);
+            if (userCount != level) {
+                System.out.println("Нерпавильная длина, попробуйте еще раз");
+                continue;
+            }
+            countTry=Check(level,countTry,userNumbers,numbers,correctString);
+        }
+    }
+
+    public static int[] Generation(int level){
+        int[] numbers = new int[level];
+        numbers[0]=(int)(Math.random()*10);
+
+        for(int i=1;i<level;i++){
+            numbers[i]=(int)(Math.random()*10);
+            for(int j=0;j<i;j++){
+                if(numbers[j]==numbers[i]){
+                    numbers[i]=(int)(Math.random()*10);
+                    j=-1;
+                }
+            }
+        }
+        return numbers;
+    }
+
+    public static String CorrectString(int level, int[] numbers){
+        String string="";
+        for(int i=0;i<level;i++){
+            string=string+String.valueOf(numbers[i]);
+        }
+        return  string;
+    }
+
+    public static int Check(int level,int countTry,int userNumbers, int[] numbers, String correctString){
+        int div, num, cows=0,bulls=0,index=0;
+        bulls = 0;
+        cows = 0;
+        countTry++;
+        for (int i = 0; i < level; i++) {
+            div = level-1;
+            num = userNumbers;
+            index = 0;
+            while (div != -1) {
+                if (numbers[i] == (int)(num / Math.pow(10, div))) {
+                    if (i == index) {
+                        bulls++;
+                    } else {
+                        cows++;
+                    }
+                }
+                num = num - (int)((int)(num / Math.pow(10, div)) * Math.pow(10, div));
+                div--;
+                index++;
+            }
+        }
+        if (bulls == level) {
+            System.out.println("Вы победили! Загаданное число - " + correctString+", количество попыток - "+countTry);
+            System.exit(0);
+        } else {
+            System.out.println("Попробуйте еще раз, количество коров - " + cows + " количество быков - " + bulls);
+        }
+        return countTry;
     }
 }
